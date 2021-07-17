e20614fb38222cd0db772e3ecfe9711818ad1672
Youngseo Kang <youngseo.kangg@gmail.com>
2021-07-18 00:38:45 +0900

    youngWeets 가져오기 기능 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
 import { dbService } from "myBase";
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 const Home = () => {
-    const [youngWeet,setYoungWeet] = useState("")
+    const [youngWeet,setYoungWeet] = useState("") // youngWeet 하나하나
+    const [youngWeets, setYoungWeets] = useState([]) // 전체 youngWeets
+    const getYoungWeets = async() => {
+        const getYWeets = await dbService.collection("youngWeets").get()
+        getYWeets.forEach((doc)=>{
+            const youngWeetsObj = {
+                ...doc.data(),
+                id: doc.id,
+            }
+            setYoungWeets((prev)=>[youngWeetsObj, ...prev])
+            console.log(youngWeets)
+        })
+    }
+    useEffect(()=>{
+        getYoungWeets();
+    },[])
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("youngWeets").add({
@@ -21,6 +36,11 @@ const Home = () => {
                 <input value={youngWeet} onChange={onChange} type="text" placeholder="what's on your mind?" maxLength={120}></input>
                 <input type="submit" value="youngWeet"></input>
             </form>
+            <ul>
+                {youngWeets.map((weet)=>{
+                    return <li key={weet.id}>{weet.youngWeet}</li>
+                })}
+            </ul>
         </div>
     )
 }
